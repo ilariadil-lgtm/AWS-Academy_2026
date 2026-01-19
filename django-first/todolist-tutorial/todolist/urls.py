1d7f07ea148a3ae03674974b399f1ee92c300e29
Ilaria Diliberto <ilaria.dil@gmail.com>
2026-01-19 23:47:47 +0100

    Add: db and migrations

--- django-first/todolist-tutorial/todolist/urls.py
+++ django-first/todolist-tutorial/todolist/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('users/', include('user.urls')),
+    path('projects/', include('project.urls')),
     path('admin/', admin.site.urls),
 ]
--- django-first/todolist-tutorial/user/urls.py
+++ django-first/todolist-tutorial/user/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path("", views.users, name="users"),
-    path("/<int:id>", views.user, name="user"),
 ] 
