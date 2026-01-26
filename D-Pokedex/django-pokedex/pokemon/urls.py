63e3e6840ac165b40f470b85e1f26c96c3437b4a
Ilaria Diliberto <ilaria.dil@gmail.com>
2026-01-27 00:14:08 +0100

    add patch and put

--- D-Pokedex/django-pokedex/pokemon/urls.py
+++ D-Pokedex/django-pokedex/pokemon/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("", views.get_pokemon_list, name="get_pokemon_list"),
     path('add_pokemon/', views.add_pokemon, name='add_pokemon'),
     #path('',admin.site.urls),
-   # path("delete/<str:id>", views.delete_pokemon, name="delete_pokemon"), 
-   # path("update/patch/<str:id>", views.update_patch_pokemon, name="update_patch_pokemon"), 
-   # path("update/put/<str:id>", views.update_put_pokemon, name="update_put_pokemon"), 
+    path("delete/<str:id>", views.delete_pokemon, name="delete_pokemon"), 
+    path("update/patch/<str:id>", views.update_patch_pokemon, name="update_patch_pokemon"), 
+    path("update/put/<str:id>", views.update_put_pokemon, name="update_put_pokemon"), 
 ]
