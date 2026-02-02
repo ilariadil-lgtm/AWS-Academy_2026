955f46dce3c1958ad6af1c227703767f3b2e4ea0
Ilaria Diliberto <ilaria.dil@gmail.com>
2026-02-02 11:20:06 +0100

    feat: add matricola models

--- django-atta/matricola/models.py
+++ django-atta/matricola/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User as AuthUser
+import uuid
 
-# Create your models here.
+
+class Matricola(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    matricola = models.TextField() # specificare il metodo di creazione di questa matricola
+    user = models.OneToOneField(AuthUser, on_delete=models.CASCADE, related_name="matricola")
+
+    class Meta:
+        db_table = "matricolas"
