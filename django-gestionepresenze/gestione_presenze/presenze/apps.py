cd38a1a77ee24927faaff9efb5cf8a32baff62ab
Ilaria Diliberto <ilaria.dil@gmail.com>
2026-02-02 10:39:12 +0100

    first commit

--- django-gestionepresenze/gestione_presenze/presenze/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PresenzeConfig(AppConfig):
-    name = 'presenze'
--- django-gestionepresenze/gestione_presenze/presenze/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RegistrazioneView
-
-urlpatterns = [
-    path('registrazione/', RegistrazioneView.as_view(), name='registrazione'),
-    # altre view per presenze/assenze
-]
